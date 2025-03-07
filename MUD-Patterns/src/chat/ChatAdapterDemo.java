e0a22f64056c1c6f87c6ec553e727765a8031e6d
MadibiyevAmir <amir.madibiyev@narxoz.kz>
2025-03-07 19:33:13 +0500

    ChatAdapterDemo.java

---
+++ MUD-Patterns/src/chat/ChatAdapterDemo.java
@@ -0,0 +1,8 @@
+public class ChatAdapterDemo {
+    public static void main(String[] args) {
+        LegacyChatService legacyService = new LegacyChatService();
+        ChatService chatAdapter = new ChatServiceAdapter(legacyService);
+
+        chatAdapter.sendMessage("Hello world!");
+    }
+}
