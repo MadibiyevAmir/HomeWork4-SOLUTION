b1dc118bf3d04132e5b06fd7a20246056f9b3cc6
MadibiyevAmir <amir.madibiyev@narxoz.kz>
2025-03-07 19:30:22 +0500

    ConfigManagerDemo.java

---
+++ MUD-Patterns/src/config/ConfigManagerDemo.java
@@ -0,0 +1,11 @@
+public class ConfigManagerDemo {
+    public static void main(String[] args) {
+        ConfigurationManager configManager = ConfigurationManager.getInstance();
+
+        System.out.println("maxPlayers: " + configManager.getConfig("maxPlayers"));
+        System.out.println("defaultLanguage: " + configManager.getConfig("defaultLanguage"));
+
+        System.out.println("\nAll Configurations:");
+        configManager.printAllConfigs();
+    }
+}
